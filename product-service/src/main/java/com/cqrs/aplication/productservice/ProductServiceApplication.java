1f189cf57351bc78e8f16154dd1532c706e441a1
Rahul Vaish <vaish.rahul4810@gmail.com>
2023-01-05 21:44:22 -0500

    Update ProductServiceApplication.java

--- product-service/src/main/java/com/cqrs/aplication/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/cqrs/aplication/productservice/ProductServiceApplication.java
@@ -1,8 +1,12 @@
 package com.cqrs.aplication.productservice;
 
+import com.cqrs.aplication.productservice.interceptors.CreateProductCommandInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ApplicationContext;
+import org.axonframework.commandhandling.CommandBus;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +16,8 @@ public class ProductServiceApplication {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
+	@Autowired
+	public void registerCreateProductCommandInterceptor(ApplicationContext context,CommandBus commandBus) {
+		commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
+	}
 }
